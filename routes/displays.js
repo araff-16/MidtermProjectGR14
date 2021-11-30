5ae9258c1794e9c2a383cbced7a40d3811780c45
Charlesdigital <charles.easton123@gmail.com>
2021-11-30 01:42:15 -0500

    add maplist view, css, favorite view

--- routes/displays.js
+++ routes/displays.js
@@ -5,41 +5,55 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 module.exports = (db) => {
-
   //RENDER LIST VIEW OF ALL MAPS
   router.get("/", (req, res) => {
-
-    const templateVars = {user_email: req.session.email}
-    res.render("index", templateVars);
+    db.query(`SELECT * FROM maps;`)
+      .then((data) => {
+        console.log(data.rows);
+        const users = data.rows;
+        res.json({ users });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+    const templateVars = { user_email: req.session.email };
+    res.render("maplist", templateVars);
   });
 
   //RENDER FAVORITES FOR PARTICULAR USER
-  router.get("/favorites", (req,res) => {
+  router.get("/favorites", (req, res) => {
     //NEED TO QUERY TO THE DATABASE TO GET ALL favorited maps for user
     //WILL STORE IN TEMPLATVARS AND SEND WITH RENDER
-    const templateVars = {user_email: req.session.email}
-    res.send("<p>FAVORIRTES PAGE</p>")
+    const templateVars = { user_email: req.session.email };
+    res.render("favorites", templateVars);
   });
 
   //RENDERS CREATE MAP PAGE
-  router.get("/createmap", (req,res) => {
-    const templateVars = {user_email: req.session.email}
+  router.get("/createmap", (req, res) => {
+    const templateVars = { user_email: req.session.email };
     res.render("create", templateVars);
   });
 
   //RENDERS VIEW MAP PAGE
-  router.get("/viewmap", (req,res) => {
-    res.send("<p>VIEW MAP PAGE</p>")
+  router.get("/viewmap", (req, res) => {
+    res.send("<p>VIEW MAP PAGE</p>");
+  });
+  // ------------------------------------------------------------------Post
+  //RENDERS MY PROFILE PAGE
+  router.post("/profile", (req, res) => {
+    const templateVars = { user_email: req.session.email };
+    res.send("<p>My Profile Page</p>");
   });
 
-  //RENDERS MY PROFILE PAGE
-  router.post("/profile", (req,res) => {
-    const templateVars = {user_email: req.session.email}
-    res.send("<p>My Profile Page</p>")
+  router.post("/favorites", (req, res) => {
+    //NEED TO QUERY TO THE DATABASE TO GET ALL favorited maps for user
+    //WILL STORE IN TEMPLATVARS AND SEND WITH RENDER
+    const templateVars = { user_email: req.session.email };
+    res.render("favorites", templateVars);
   });
 
   return router;
